14320abfc5d635e484f0ce5da1a58f8ad4505e7c
Junepil Lee <hanu9257@gmail.com>
2025-10-22 22:45:10 +0800

    feat: implement IOhander class and test
    - `IOHandler.js`: Formatting and parsing IO using given library
    - `IOHandlerTest.js`: Test file that checks the senity of `IOHandler.js`

---
+++ __tests__/IOHandlerTest.js
@@ -0,0 +1,94 @@
+import { getCarNames, getTurns, printGameHistory, printWinner } from "../src/IOHandler";
+import { Console } from "@woowacourse/mission-utils";
+
+const ERROR_STRING = '[ERROR]';
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  })
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('IO handler', () => {  
+  describe("getCarNames", () => {
+    test('success', async () => {
+      const inputs = ['sam', 'sam,alex,tom'];
+      const outputs = [['sam'], ['sam', 'alex', 'tom']];
+
+      mockQuestions(inputs);
+      for (const output of outputs) {
+        const result = await getCarNames();
+        
+        expect(result).toEqual(output);  
+      }
+    });
+    test('fail', async () => {
+      const inputs = [
+        '',
+        'pineapple, apple',
+        '엄청큰자동차, 엄청빠른자동차',
+        'alex,,bob,'
+      ];
+
+      mockQuestions(inputs);
+      for (let i = 0; i < inputs.length; i++) { // will this work?
+        await expect(getCarNames()).rejects.toThrow(ERROR_STRING);
+      }
+    });
+  })
+  describe('getTurns', () => {
+    test('success', async () => {
+      const inputs = ['4'];
+      const output = 4;
+      mockQuestions(inputs);
+
+      const result = await getTurns();
+      expect(result).toBe(output)
+    })
+    test('fail', async () => {
+      const inputs = ['0', '-3', '3.14', 'twice'];
+      mockQuestions(inputs)
+
+      for (const input of inputs) {
+        await expect(getTurns()).rejects.toThrow(ERROR_STRING)
+      }
+    })
+  })
+  test('printWinner', () => {
+    const input = ['john', 'sam'];
+    const output = 'john, sam';
+
+    const logSpy = getLogSpy();
+    printWinner(input);
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+  })
+  test('printGameHistory', () => {
+    const input = [
+      [
+        {name: 'john',position: 1},
+        {name: 'sam',position: 2},
+      ],
+      [
+        {name: 'john',position: 2},
+        {name: 'sam',position: 3},
+      ]
+    ];
+    const outputs = ['john : -', 'sam : --', 'john : --', 'sam : ---'];
+    const logSpy = getLogSpy();
+    printGameHistory(input);
+
+    for (const output of outputs) {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    }
+  })
+});
---
+++ src/IOHandler.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-restricted-syntax */
+import { Console } from '@woowacourse/mission-utils';
+import { CustomError, InvalidCarNameError, InvalidTurnError } from './error';
+
+function parseCars(input) {
+  const cars = input.split(',');
+
+  if (cars.length === 0)
+    throw new CustomError('There most be at least one car in the race');
+
+  for (const car of cars) {
+    if (car.length > 5)
+      throw new InvalidCarNameError();
+    if (car.length === 0)
+      throw new InvalidCarNameError('Car name can not be blank');
+  }
+
+  return cars;
+}
+
+async function getCarNames() {
+  const input = await Console.readLineAsync(
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  );
+  
+  const cars = parseCars(input);
+  
+  return cars;
+}
+
+function parseTurns(input) {
+  const turns = parseInt(input, 10);
+
+  if (turns <= 0)
+    throw new InvalidTurnError();
+
+  return turns;
+}
+
+async function getTurns() {
+  const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+  const turns = parseTurns(input);
+
+  return turns;
+}
+
+function printWinner(winner) {
+  const winnerString = winner.join(", ");
+  
+  Console.print(`최종 우승자 : ${winnerString}\n`);
+}
+
+function printGameHistory(gameHistory) {
+  Console.print('실행 결과\n');
+
+  for (const turn of gameHistory) {
+    for (const { name, position } of turn) {
+      const track = '-'.repeat(position);
+  
+      Console.print(`${name} : ${track}\n\n`);
+    }
+  }
+}
+
+export { getCarNames, getTurns, printWinner, printGameHistory };
