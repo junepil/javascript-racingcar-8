66b68fcee57a75aab3580857d32843bbcd5b5198
Junepil Lee <hanu9257@gmail.com>
2025-10-25 15:10:39 +0800

    feat: implement game play in `App.js`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Game from './Game.js';
+import {
+  getCarNames,
+  getTurns,
+  printGameHistory,
+  printWinner,
+} from './IOHandler.js';
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await getCarNames();
+    const turns = await getTurns();
+    const game = new Game(carNames, turns);
+
+    game.play();
+
+    printGameHistory(game.history);
+    printWinner(game.getWinner());
+  }
 }
 
 export default App;
